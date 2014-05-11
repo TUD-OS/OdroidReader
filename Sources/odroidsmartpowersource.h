1ca2c41decf051d256143fbdc5dc5804e717d595
Marcus Haehnel <marcus@mh-development.info>
2014-05-11 19:58:40 +0200

    More Device support

--- Sources/odroidsmartpowersource.h
+++ Sources/odroidsmartpowersource.h
@@ -2,17 +2,20 @@
 #define ODROIDSMARTPOWERSOURCE_H
 
 #include <Data/datasource.h>
+#include <Data/datadescriptor.h>
 #include <hidapi/hidapi.h>
 
 class OdroidSmartPowerSource : public DataSource
 {
+    bool _running, reconnect;
     QString _path;
     hid_device* device;
+    QVector<const DataDescriptor*> descs;
 public:
     OdroidSmartPowerSource(const char* path);
     QString path() { return _path; }
     bool canExecute() const { return false; }
-    bool isRunning() const { return false; }
+    bool isRunning() const { return _running; }
     QString descriptor();
 
 public slots:
