67424522597dc0980d315c88f68671f1157aa009
Marcus Haehnel <mhaehnel@tudos.org>
2014-06-13 09:59:14 +0200

    Fixed crash on invalid device

--- smartpowermonitor.cpp
+++ smartpowermonitor.cpp
@@ -26,13 +26,15 @@ void SmartPowerMonitor::checkDevices() {
 			if (s->path() == info.path) {
                 assert(toDelete.contains(s));
                 toDelete.removeAll(s);
-                assert(!toDelete.contains(s));
+				assert(!toDelete.contains(s));
                 newEntry = false;
                 break;
             }
         }
         if (newEntry) {
-			sources.append(new OdroidSmartPowerSource(info.path.toStdString().c_str()));
+			OdroidSmartPowerSource* sps = new OdroidSmartPowerSource(info.path.toStdString().c_str());
+			if (!sps->good()) continue;
+			sources.append(sps);
             emit addSource(sources.last());
 			qDebug() << "Serial      : " << info.serial_number;
 			qDebug() << "Manufacturer: " << info.manufacturer_string;
